ebe2e20707115701df35095acb51b6c2f235e4b4
Ezio <nxz84@case.edu>
2019-05-09 22:36:59 -0500

    mongodb able to query

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate actix_web;
 extern crate listenfd;
 extern crate mongodb;
+extern crate serde_json;
 
 use listenfd::ListenFd;
-use actix_web::{server, App, HttpRequest, Json, Result, http::Method, Responder};
+use actix_web::{server, App, HttpRequest, HttpResponse, Json, Result, http::Method, Responder};
 
 use mongodb::{Bson, Document};
 use mongodb::{Client, ThreadedClient};
@@ -29,19 +30,24 @@ fn index(req: &HttpRequest) -> Result<Json<User>> {
     Ok(Json(User {email: "saiop147@gmail.com".to_string(), username: "admin".to_string()}))
 }
 
-// fn get_reports(req: &HttpRequest) -> impl Responder {
-//     let client = Client::connect("localhost", 27017)
-//     .expect("Failed to initialize standalone client.");
-//     let coll = client.db("lighters").collection("reports");
-//     let mut cursor = coll.find(None, None).unwrap();
-// }
+fn get_reports(req: &HttpRequest) -> HttpResponse {
+    let client = Client::connect("localhost", 27017)
+    .expect("Failed to initialize standalone client.");
+    let coll = client.db("lighters").collection("reports");
+    let cursor = coll.find(None, None).unwrap();
+    let docs: Vec<_> = cursor.map(|doc| doc.unwrap()).collect();
+    let serialized = serde_json::to_string(&docs).unwrap();
+    HttpResponse::Ok()
+                .body(serialized)
+}
 
 fn main() {
     let mut listenfd = ListenFd::from_env();
     let mut server = server::new(|| {
         App::new()
             .prefix("/api")
-            .resource("/", |r| r.f(index))  
+            .resource("/", |r| r.f(index))
+            .resource("/reports", |r| r.f(get_reports))  
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
